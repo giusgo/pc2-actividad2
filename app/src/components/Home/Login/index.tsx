ecd7c35731072ad051c8dc7d125e6dec01318253
giusgo <giuseppeg@uninorte.edu.co>
2024-05-23 18:40:52 +0000

    Login form almost done

--- app/src/components/Home/Login/index.tsx
+++ app/src/components/Home/Login/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     Card,
     CardContent,
@@ -9,8 +11,51 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+const formSchema = z.object({
+    username: z.string()
+        .min(2, {
+            message: "El usuario debe ser de mínimo 2 caracteres.",
+        })
+        .max(20, {
+            message: "El usuario debe ser de máximo 20 caracteres."
+        }),
+    password: z.string()
+        .min(8, {
+            message: "La contraseña debe ser de mínimo 8 caracteres.",
+        })
+        .max(12, {
+            message: "La contraseña debe ser de máximo 12 caracteres."
+        })
+})
 
 export default function Login() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: ""
+        },
+    });
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Print
+        console.log(values)
+    }
+
     return <Card>
         <CardHeader>
             <CardTitle>Ingreso</CardTitle>
@@ -18,18 +63,40 @@ export default function Login() {
                 Ingresa tus credenciales de profesor en los siguientes campos.
             </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-2">
-            <div className="space-y-1">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" />
-            </div>
-            <div className="space-y-1">
-                <Label htmlFor="username">Username</Label>
-                <Input id="username" />
-            </div>
-        </CardContent>
-        <CardFooter>
-            <Button>Save changes</Button>
-        </CardFooter>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <CardContent className="space-y-2">
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Usuario</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Contraseña</FormLabel>
+                                <FormControl>
+                                    <Input type="password" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </CardContent>
+                <CardFooter>
+                    <Button type="submit">Enviar</Button>
+                </CardFooter>
+            </form>
+        </Form>
     </Card>
 }
