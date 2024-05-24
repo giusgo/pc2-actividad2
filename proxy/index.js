0e9c02d9005d0aebc06cee98411d1c199cded70f
giusgo <giuseppeg@uninorte.edu.co>
2024-05-24 08:42:24 +0000

    ahora lo q fue fue

--- proxy/index.js
+++ proxy/index.js
@@ -1,15 +1,18 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
+const proxiedAuth = process.env.PROXIED_AUTH || 'localhost';
+const proxiedBackend = process.env.PROXIED_BACKEND || 'localhost';
+
 // Create a proxy server instance for port 3000
-const proxy1 = httpProxy.createProxyServer({ target: 'http://localhost:3000' });
+const proxy1 = httpProxy.createProxyServer({ target: `http://${proxiedAuth}:3000` });
 
 // Create a proxy server instance for port 4000
-const proxy2 = httpProxy.createProxyServer({ target: 'http://localhost:4000' });
+const proxy2 = httpProxy.createProxyServer({ target: `http://${proxiedBackend}:4000` });
 
 // Create a server to listen on port 8000 and proxy requests to port 3000
 http.createServer(function (req, res) {
-    proxy1.web(req, res, function(err) {
+    proxy1.web(req, res, function (err) {
         console.error('Error proxying to port 3000:', err);
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Something went wrong');
@@ -18,7 +21,7 @@ http.createServer(function (req, res) {
 
 // Create a server to listen on port 9000 and proxy requests to port 4000
 http.createServer(function (req, res) {
-    proxy2.web(req, res, function(err) {
+    proxy2.web(req, res, function (err) {
         console.error('Error proxying to port 4000:', err);
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Something went wrong');
