03e504c753f3633e4cb0efd786ca263eea5a8651
giusgo <giuseppeg@uninorte.edu.co>
2024-05-24 04:28:19 +0000

    Just in case

--- app/src/app/api/auth/[...nextauth]/route.ts
+++ app/src/app/api/auth/[...nextauth]/route.ts
@@ -34,7 +34,7 @@ const handler = NextAuth({
             },
         }),
     ],
-
+    secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import Background from "@/components/Home/background";
 import Navbar from "@/components/General/Navbar";
 import TeacherForm from "@/components/Home/form";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (session) {
+    redirect("/reportes");
+  }
   return (
     <main>
       <Background />
